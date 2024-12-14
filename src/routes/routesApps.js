934362480b8b51999cb8b68ad3ff9a70750724b5
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-14 18:23:43 -0500

    fix bug

--- src/routes/routesApps.js
+++ src/routes/routesApps.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const LandingPage = React.lazy(() => import('src/views/landingPage/index'))
+// const LandingPage = React.lazy(() => import('src/views/landingPage/index'))
 const Dashboard = React.lazy(() => import('src/views/dashboard/index'))
 
 // About
 const About = React.lazy(() => import('src/views/about/index'))
 
 const routes = [
-  { path: '/landingPage', name: 'Landing Page', element: LandingPage },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/about', name: 'About', element: About },
 ]
 
 export default routes
+
+// { path: '/landingPage', name: 'Landing Page', element: LandingPage },
